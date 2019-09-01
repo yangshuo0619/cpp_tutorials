c96604df82204aec557d698b44ca33afc13ef205
jiziye <jiziye2016@163.com>
2019-09-01 07:41:13 -0700

    wrong demo

---
+++ class-demo/test.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<assert.h>
+
+using namespace std;
+
+class A
+{
+  public:
+    int a;
+    A(){
+      a1=1;
+      a2=2;
+      a3=3;
+      a=4;}
+   int fun()
+  {
+    cout<<a<<endl;
+    cout<<a1<<endl;
+    cout<<a2<<endl;
+    cout<<a3<<endl;
+  
+    return 1;
+   
+ }
+
+  public:
+    int a1;
+  protected:
+    int a2;
+  private:
+    int a3;
+};
+
+class B : private A
+{
+  public:
+    int a;
+    B(int i){
+    A();
+    a=i;}
+
+  void fun()
+  {
+    cout<<a<<endl;
+    cout<<a1<<endl;
+    cout<<a2<<endl;
+  // cout<<a3<<endl;
+  }
+};
+
+int main()
+{
+  B b(10);
+  cout<<b.a<<endl;
+  //cout<<b.a1<<endl;
+  //cout<<b.a2<<endl;
+  //cout<<b.a3<<endl;
+  
+  b.fun();
+  cout<<b.A::fun();
+
+  return 0;
+}
+
+
+
+
+
+
