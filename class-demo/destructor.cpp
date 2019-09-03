9ef3b0192d09a9dfe9ae3e819c70e05b984e9ef4
jiziye <jiziye2016@163.com>
2019-09-03 20:33:37 +0800

    add demo

---
+++ class-demo/destructor.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+
+using namespace std;
+
+class Line
+{
+  public:
+    void setLength(double len);
+    double getLength(void);
+    Line();
+    ~Line();
+  
+  private:
+    double length;
+};
+
+Line::Line(void)
+{
+  cout<<"object is being created"<<endl;
+}
+
+Line::~Line(void)
+{
+  cout<<"object is being deleted"<<endl;
+}
+
+
+void Line::setLength(double len)
+{
+  length=len;
+}
+
+double Line::getLength(void)
+{
+  return length;
+}
+
+int main()
+{
+  Line line;
+ 
+  line.setLength(50.0);
+  cout<<"length of line:"<< line.getLength()<<endl;
+ 
+  return 0;
+
+}
+
+
+
