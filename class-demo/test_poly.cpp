0a52023be63d16982130c2b8cbddffb11ec266ae
jiziye <jiziye2016@163.com>
2019-09-09 08:51:54 -0700

    add demo

---
+++ class-demo/test_poly.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+
+using namespace std;
+
+class Shape
+{
+  protected:
+    int width,height;
+  public:
+    Shape(int a=0, int b=0)
+    {
+      width=a;
+      height=b;
+    }
+
+  virtual int area()
+  {
+    cout<<"parent class area: "<<endl;
+    return 0;
+  }
+
+};
+ 
+class Rectangle: public Shape
+{
+  public:
+    Rectangle( int a=0,int b=0):Shape(a,b){}
+    int area()
+    {
+      cout<<"rectangle class area: "<<endl;
+      return (width*height);
+    }
+};
+
+class Triangle: public Shape
+{
+  public:
+    Triangle(int a=0,int b=0):Shape(a,b){}
+    int area()
+    {
+      cout<<"triangle class area: "<<endl;
+      return (width*height/2);
+    }
+};
+
+
+int main()
+{
+ // Shape shape;
+  Rectangle rec(10,7);
+  Triangle tri(10,5);
+
+  Shape & shape=rec;
+  cout<<shape.area()<<endl;
+
+  Shape & shape1=tri;
+  cout<<shape1.area()<<endl;
+
+  return 0;
+
+}
+
+
