d2a0c7d9793a788b2c94d588168d0631bd5e6694
ziyeji <ziyeji@slightech.com>
2020-06-28 17:28:47 +0800

    modify C++_scope_oper.cpp

--- demo/C++_scope_oper.cpp
+++ demo/C++_scope_oper.cpp
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<iostream>
 
 int value = 0;
 
@@ -6,14 +7,14 @@ void printvalue(){
     printf("value =  %d; \n",value);
 };
 
-int main()
+int main()//main函数内不能再定义其他函数，即C++不支持函数的嵌套定义，但是函数的嵌套调用可以
 {
-    int value = 0;
-    value = 1;
-    printvalue();
 
-    ::value = 2;
-    printvalue();
+    int value = 1;
+    printvalue();//打印全局变量value = 0;
+
+    ::value = 2;//:: 是域操作符，可以直接操作全局变量;
+    printvalue();//打印全局变量value = 2;
 
     return 0;
 }
